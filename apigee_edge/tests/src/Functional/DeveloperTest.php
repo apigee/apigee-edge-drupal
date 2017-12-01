e317016e7f561c72e8ad0e98d7d7c167207292db
peterserfozo <peter.serfozo@pronovix.com>
2017-12-01 11:56:52 +0100

    Added functional tests.

--- apigee_edge/tests/src/Functional/DeveloperTest.php
+++ apigee_edge/tests/src/Functional/DeveloperTest.php
@@ -13,11 +13,11 @@ use Drupal\Tests\BrowserTestBase;
 class DeveloperTest extends BrowserTestBase {
 
   /**
-   * SDKConnector object.
+   * The DeveloperController object.
    *
-   * @var \Drupal\apigee_edge\SDKConnector
+   * @var \Apigee\Edge\Api\Management\Controller\DeveloperController
    */
-  protected $sdkConnector;
+  protected $developerController;
 
   public static $modules = [
     'apigee_edge',
@@ -28,7 +28,7 @@ class DeveloperTest extends BrowserTestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->sdkConnector = \Drupal::service('apigee_edge.sdk_connector');
+    $this->developerController = \Drupal::service('apigee_edge.sdk_connector')->getDeveloperController();
   }
 
   /**
@@ -48,6 +48,7 @@ class DeveloperTest extends BrowserTestBase {
    * if the Drupal user registered by the admin.
    */
   public function testDeveloperRegisteredByAdmin() {
+    // Create blocked user by the admin.
     $this->drupalLogin($this->rootUser);
     $this->drupalGet('/admin/people/create');
 
@@ -72,15 +73,48 @@ class DeveloperTest extends BrowserTestBase {
 
     $this->submitForm($formdata, 'Create new account');
 
-    $dc = $this->sdkConnector->getDeveloperController();
     /** @var Developer $developer */
-    $developer = $dc->load($test_user['email']);
+    $developer = $this->developerController->load($test_user['email']);
 
     $this->assertEquals($developer->getEmail(), $test_user['email']);
     $this->assertEquals($developer->getFirstName(), $test_user['first_name']);
     $this->assertEquals($developer->getLastName(), $test_user['last_name']);
     $this->assertEquals($developer->getUserName(), $test_user['username']);
     $this->assertEquals($developer->getStatus(), $developer::STATUS_INACTIVE);
+
+    // Unblock and edit the user's first name, last name by the admin.
+    $this->drupalGet('/user/2/edit');
+    $test_user['first_name'] = '(mod) Functional';
+    $test_user['last_name'] = '(mod) Test';
+    $test_user['status'] = '1';
+
+    $formdata['first_name[0][value]'] = $test_user['first_name'];
+    $formdata['last_name[0][value]'] = $test_user['last_name'];
+    $formdata['status'] = $test_user['status'];
+
+    $this->submitForm($formdata, 'Save');
+
+    $developer = $this->developerController->load($test_user['email']);
+
+    $this->assertEquals($developer->getEmail(), $test_user['email']);
+    $this->assertEquals($developer->getFirstName(), $test_user['first_name']);
+    $this->assertEquals($developer->getLastName(), $test_user['last_name']);
+    $this->assertEquals($developer->getUserName(), $test_user['username']);
+    $this->assertEquals($developer->getStatus(), $developer::STATUS_ACTIVE);
+
+    // Block the user's account on the people page.
+    //$this->drupalGet('/admin/people');
+    /*$this->getSession()->getPage()->selectFieldOption('edit-action', 'user_block_user_action');
+    $this->getSession()->getPage()->checkField('edit-user-bulk-form-1');
+    $this->getSession()->getPage()->pressButton('edit-submit');
+
+    $developer = $this->developerController->load($test_user['email']);
+    $this->assertEquals($developer->getStatus(), $developer::STATUS_INACTIVE);
+
+    // Delete user by admin.
+    $this->drupalGet('/user/2/cancel');
+    $this->getSession()->getPage()->checkField('edit-user-cancel-method-user-cancel-delete');
+    $this->getSession()->getPage()->pressButton('edit-submit');*/
   }
 
 }
---
+++ apigee_edge/tests/src/Functional/EdgeAccountTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\Tests\apigee_edge\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Edge account related tests.
+ *
+ * @group ApigeeEdge
+ */
+class EdgeAccountTest extends BrowserTestBase {
+
+  /**
+   * Credential storage.
+   *
+   * @var array
+   */
+  protected $credentials = [];
+
+  /**
+   * User with administrator permissions for the edge module.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $adminUser = NULL;
+
+  public static $modules = [
+    'apigee_edge',
+  ];
+
+  /**
+   * Initializes the credentials property.
+   *
+   * @return bool
+   */
+  protected function initCredentials() : bool {
+    if (($username = getenv('APIGEE_EDGE_USERNAME'))) {
+      $this->credentials['username'] = $username;
+    }
+    if (($password = getenv('APIGEE_EDGE_PASSWORD'))) {
+      $this->credentials['password'] = $password;
+    }
+    if (($organization = getenv('APIGEE_EDGE_ORGANIZATION'))) {
+      $this->credentials['organization'] = $organization;
+    }
+    if (($base_url = getenv('APIGEE_EDGE_BASE_URL'))) {
+      $this->credentials['base_url'] = $base_url;
+    }
+
+    return (bool) $this->credentials;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    if (!$this->initCredentials()) {
+      $this->markTestSkipped('credentials not found');
+    }
+    parent::setUp();
+
+    $this->adminUser = $this->createUser(['administer apigee edge']);
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests validating and saving the apigee edge account credentials.
+   */
+  public function testAccount() {
+    $this->drupalGet('/admin/config/apigee_edge');
+
+    $formdata = [
+      'credentials_storage_type' => 'credentials_storage_private_file',
+      'credentials_api_organization' => $this->credentials['organization'],
+      'credentials_api_base_url' => $this->credentials['base_url'],
+      'credentials_api_username' => $this->credentials['username'],
+      'credentials_api_password' => $this->credentials['password'],
+    ];
+
+    $this->submitForm($formdata, t('Send request'));
+    $this->assertSession()->pageTextContains(t('Connection successful'));
+
+    $this->submitForm($formdata, t('Save configuration'));
+    $this->assertSession()->pageTextContains(t('The configuration options have been saved'));
+  }
+
+  public function testEnvStorage() {
+    $this->drupalGet('/admin/config/apigee_edge');
+
+    $formdata = [
+      'credentials_storage_type' => 'credentials_storage_env',
+    ];
+
+    $this->submitForm($formdata, t('Send request'));
+    $this->assertSession()->pageTextContains(t('Connection successful'));
+
+    $this->submitForm($formdata, t('Save configuration'));
+    $this->assertSession()->pageTextContains(t('The configuration options have been saved'));
+  }
+
+}
