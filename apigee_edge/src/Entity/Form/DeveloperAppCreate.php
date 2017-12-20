295346e52eb8152c5610f3d450e7c2ea67ab31e6
Tamás Demeter-Haludka <yorirou@gmail.com>
2017-12-20 17:17:08 +0100

    Adds widgets to the developer app create form.

--- apigee_edge/src/Entity/Form/DeveloperAppCreate.php
+++ apigee_edge/src/Entity/Form/DeveloperAppCreate.php
@@ -11,10 +11,49 @@ class DeveloperAppCreate extends EntityForm {
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
+    $config = \Drupal::config('apigee_edge.createapp');
+
+    /** @var \Drupal\apigee_edge\Entity\DeveloperApp $app */
+    $app = $this->entity;
+
     $form['details'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Details'),
       '#collapsible' => FALSE,
+    ];
+
+    $form['details']['displayName'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Application Name'),
+      '#required' => TRUE,
+      '#default_value' => $app->getDisplayName(),
+    ];
+
+    $form['details']['name'] = [
+      '#type' => 'machine_name',
+      '#machine_name' => [
+        'source' => ['details', 'displayName'],
+        'label' => $this->t('Internal name'),
+      ],
+      '#title' => $this->t('Internal name'),
+      '#disabled' => !$app->isNew(),
+      '#default_value' => $app->getName(),
+    ];
+
+    $form['details']['callbackUrl'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Callback URL'),
+      '#default_value' => $app->getCallbackUrl(),
+      '#access' => (bool) $config->get('callback_url_visible'),
+      '#required' => (bool) $config->get('callback_url_required'),
+    ];
+
+    $form['details']['description'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Description'),
+      '#default_value' => $app->getDescription(),
+      '#access' => (bool) $config->get('description_visible'),
+      '#required' => (bool) $config->get('description_required'),
     ];
 
     return parent::form($form, $form_state);
