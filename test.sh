02ca17c1fa19694f45a58ef9bb5198280c9e2340
Dezső Biczó <mxr576@gmail.com>
2018-01-29 12:47:10 +0100

    Separate Apigee Edge related Docker environments from the general testing framework.

--- test.sh
+++ test.sh
@@ -8,11 +8,12 @@ set -e
 # is 0.1.5, which does not have the fix for this problem: https://github.com/phpDocumentor/TypeResolver/pull/16.
 # We have to update Drush too, because 9.0.0-beta4 installed by Drupal project only allows
 # phpdocumentor/reflection-docblock < 3.0.
-composer require phpspec/prophecy:^1.6.1
-composer require phpdocumentor/reflection-docblock:^3.1.0
 # apigee/edge dev-2.x-dev conflicts with guzzlehttp/psr7[1.3.1].
-composer require guzzlehttp/psr7:^1.4.1
+composer require phpspec/prophecy:^1.6.1 phpdocumentor/reflection-docblock:^3.1.0 guzzlehttp/psr7:^1.4.1
+# After we have upgraded the dependencies to the highest possible then we can try to downgrade them to the
+# lowest possible. Without the first upgrade this does not work.
+composer require ${DEPENDENCIES} phpspec/prophecy:^1.6.1 phpdocumentor/reflection-docblock:^3.1.0 guzzlehttp/psr7:^1.4.1
 composer config repositories.library path /opt/drupal-module
-composer require $DEPENDENCIES "drupal/${DRUPAL_MODULE_NAME}"
+composer require ${DEPENDENCIES} "drupal/${DRUPAL_MODULE_NAME}"
 composer show
 php vendor/bin/phpunit -c core --group apigee_edge -v --debug
