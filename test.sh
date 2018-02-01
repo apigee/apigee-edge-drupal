2891e41f79ae7b573c87e77642eb57e65981dcfa
Dezső Biczó <mxr576@gmail.com>
2018-02-01 11:47:11 +0100

    No need to downgrade phpdocumentor/reflection-docblock on highest tests.

--- test.sh
+++ test.sh
@@ -4,9 +4,7 @@ set -e
 
 # This library has to be updated to the latest version, because the lowest installed 2.0.4 is in conflict with
 # one of the Apigee PHP SDK's required library's (symfony/property-info:^3.2) minimum requirement.
-if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then \
-  composer require phpdocumentor/reflection-docblock:3.1.0; else \
-  composer require phpdocumentor/reflection-docblock:^3.1.0; fi
+if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then composer require phpdocumentor/reflection-docblock:3.1.0; fi;
 composer config repositories.library path /opt/drupal-module
 composer require ${DEPENDENCIES} "drupal/${DRUPAL_MODULE_NAME}"
 # Install this to get more detailed output from PHPUnit.
