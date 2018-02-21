705987f10fe155f6c755a835f371b78c9aebf1e2
Dezső Biczó <mxr576@gmail.com>
2018-02-21 14:02:15 +0100

    Fix developer to drupal user synchronisation job by calling validation on the created user entity and suppressing DeveloperEmailUniqueValidator .

--- src/Job/CreateUser.php
+++ src/Job/CreateUser.php
@@ -20,7 +20,10 @@
 namespace Drupal\apigee_edge\Job;
 
 use Drupal\apigee_edge\Entity\Developer;
+use Drupal\apigee_edge\Plugin\Validation\Constraint\DeveloperEmailUniqueValidator;
+use Drupal\Core\Entity\EntityMalformedException;
 use Drupal\user\Entity\User;
+use Drupal\user\Plugin\Validation\Constraint\UserNameUnique;
 
 /**
  * A job to create a Drupal user from an Apigee Edge developer.
@@ -48,14 +51,29 @@ class CreateUser extends EdgeJob {
   protected function executeRequest() {
     /** @var \Drupal\apigee_edge\Entity\Developer $developer */
     $developer = Developer::load($this->mail);
-    User::create([
+    $user = User::create([
       'name' => $developer->getUserName(),
       'mail' => $developer->getEmail(),
       'first_name' => $developer->getFirstName(),
       'last_name' => $developer->getLastName(),
       'status' => $developer->getStatus() === Developer::STATUS_ACTIVE,
       'pass' => user_password(),
-    ])->save();
+    ]);
+    // Whitelist developer's email address because we know that it exists
+    // on Edge and we would like to create a new user for it in Drupal.
+    DeveloperEmailUniqueValidator::whitelist($developer->getEmail());
+    $violations = $user->validate();
+    /** @var \Drupal\Core\Entity\EntityConstraintViolationList $userNameViolations */
+    $userNameViolations = $violations->getByField('name');
+    foreach ($userNameViolations as $violation) {
+      // Throw an exception if username is already taken here instead
+      // of getting a database exception in a lower layer.
+      /** @var \Symfony\Component\Validator\ConstraintViolation $violation */
+      if (get_class($violation->getConstraint()) === UserNameUnique::class) {
+        throw new EntityMalformedException((string) $violation->getMessage());
+      }
+    }
+    $user->save();
   }
 
   /**
