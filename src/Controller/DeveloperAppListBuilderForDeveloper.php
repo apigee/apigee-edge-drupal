85159775fb0712d7744a7d654c9ad4b2988a27a6
peterserfozo <peter.serfozo@pronovix.com>
2018-03-06 10:16:20 +0100

    Anlytics operation link for developers.

--- src/Controller/DeveloperAppListBuilderForDeveloper.php
+++ src/Controller/DeveloperAppListBuilderForDeveloper.php
@@ -122,8 +122,8 @@ class DeveloperAppListBuilderForDeveloper extends DeveloperAppListBuilder {
   /**
    * {@inheritdoc}
    */
-  protected function getDeveloperAppAnalyticsLinkUrl(EntityInterface $entity) {
-    return $entity->toUrl('analytics-for-developer');
+  protected function getDeveloperAppAnalyticsLink() {
+    return 'analytics-for-developer';
   }
 
   /**
