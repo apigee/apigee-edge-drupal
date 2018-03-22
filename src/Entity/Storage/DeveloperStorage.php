8a956a0a64131484cda926ac033af7eee1aac189
Dezső BICZÓ <mxr576@gmail.com>
2018-03-22 09:35:35 +0100

    CS fix.

--- src/Entity/Storage/DeveloperStorage.php
+++ src/Entity/Storage/DeveloperStorage.php
@@ -152,7 +152,7 @@ class DeveloperStorage extends EdgeEntityStorageBase implements DeveloperStorage
   protected function getPersistentCacheTags(EntityInterface $entity) {
     /** @var \Drupal\apigee_edge\Entity\Developer $entity */
     $cacheTags = parent::getPersistentCacheTags($entity);
-    $cacheTags = array_map(function($cid) use ($entity) {
+    $cacheTags = array_map(function ($cid) use ($entity) {
       // Sanitize accented characters in developer's email addresses.
       return str_replace($entity->id(), filter_var($entity->id(), FILTER_SANITIZE_ENCODED), $cid);
     }, $cacheTags);
