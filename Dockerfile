015739a60fcde4bf2f4dce3aa360eeb4a5383cc4
Dezső BICZÓ <mxr576@gmail.com>
2018-03-23 10:12:52 +0100

    Updated docker-compose configuration to support using different database backends for each builds. Also tweaked the build process to leverage cache feature of Travis for Composer packages.

--- Dockerfile
+++ Dockerfile
@@ -1,48 +1,12 @@
-ARG PHP_VERSION="7.1"
-ARG PHP_IMAGE_VERSION="-dev-4.1.1"
+ARG PHP_IMAGE="wodby/drupal-php:7.1-dev-4.2.2"
 
-FROM wodby/drupal-php:${PHP_VERSION}${PHP_IMAGE_VERSION}
+FROM ${PHP_IMAGE}
 
 USER root
 
 # For manipulating JSON files if necessary.
 RUN apk add --update jq
 
-USER wodby
-
-ARG DRUPAL_CORE="8.5"
-ENV DRUPAL_CORE=${DRUPAL_CORE}
-ARG DEPENDENCIES=""
-ENV DEPENDENCIES=${DEPENDENCIES}
-
-ENV COMPOSER_OPTIONS="/var/www/html --no-interaction"
-
-# We are using drupal-composer/drupal-project instead of drupal/drupal because we would like to update all
-# libraries, including Drupal, to the latest version when doing "highest" testing.
-RUN if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then \
-  composer create-project drupal/drupal:$DRUPAL_CORE $COMPOSER_OPTIONS; else \
-  composer create-project drupal/drupal:^$DRUPAL_CORE $COMPOSER_OPTIONS && composer update -o --with-dependencies; \
-  fi
-
-USER root
-
-# Based on https://www.drupal.org/node/244924.
-RUN chown -R wodby:www-data . \
-    && find . -type d -exec chmod 6750 '{}' \; \
-    && find . -type f -exec chmod 0640 '{}' \; \
-    # Fix permissions on directory and .htaccess file.
-    && chmod 755 . \
-    && chmod 644 .htaccess
-
-RUN mkdir -p /var/www/html/sites/default/files \
-    && chown -R wodby:www-data /var/www/html/sites/default/files \
-    && chmod 6770 /var/www/html/sites/default/files
-
-# Create simpletest directory with correct permissions.
-RUN mkdir -p /var/www/html/sites/simpletest \
-    && chown -R www-data:wodby /var/www/html/sites/simpletest \
-    && chmod 6750 /var/www/html/sites/simpletest
-
-USER wodby
-
 COPY --chown=wodby:www-data . /opt/drupal-module
+
+USER wodby
--- test.sh
+++ test.sh
@@ -1,7 +1,46 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
+
+set -e
+
+# We mounted the cache/files folder from the host so we have to fix permissions
+# on the parent cache folder because it did not exist before.
+sudo -u root sh -c "chown -R wodby:wodby /home/wodby/.composer/cache"
 
 # Ensure that we are in the correct directory.
-cd /var/www/html
+mkdir -p /var/www/html/web && cd /var/www/html/web
+
+COMPOSER_OPTIONS="/var/www/html/web --no-interaction --prefer-dist"
+
+# We are using drupal-composer/drupal-project instead of drupal/drupal because we would like to update all
+# libraries, including Drupal, to the latest version when doing "highest" testing.
+if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then \
+  composer create-project drupal/drupal:${DRUPAL_CORE} ${COMPOSER_OPTIONS}; else \
+  composer create-project drupal/drupal:^${DRUPAL_CORE} ${COMPOSER_OPTIONS} && composer update -o --with-dependencies; \
+  fi
+
+# Based on https://www.drupal.org/node/244924.
+# Also fix permissions on directory and .htaccess file.
+sudo -u root sh -c "chown -R wodby:www-data . \
+    && find . -type d -exec chmod 6750 '{}' \; \
+    && find . -type f -exec chmod 0640 '{}' \; \
+    && chmod 755 . \
+    && chmod 644 .htaccess"
+
+sudo -u root sh -c "mkdir -p /var/www/html/web/sites/default/files \
+    && chown -R wodby:www-data /var/www/html/web/sites/default/files \
+    && chmod 6770 /var/www/html/web/sites/default/files"
+
+# Make sure that the log folder is writable for both www-data and wodby users.
+sudo -u root sh -c "chown -R www-data:wodby /mnt/files/log \
+ && chmod -R 6750 /mnt/files/log \
+ && mkdir -p /mnt/files/log/simpletest \
+ && chown -R www-data:wodby /mnt/files/log/simpletest \
+ && chmod 6750 /mnt/files/log/simpletest"
+
+# Change location of the simpletest folder, because it seems even if
+# BROWSERTEST_OUTPUT_DIRECTORY is set the html output is printed out to
+# https://github.com/drupal/core/blob/8.5.0/tests/Drupal/Tests/BrowserTestBase.php#L1086
+ln -s /mnt/files/log/simpletest /var/www/html/web/sites/simpletest
 
 # This library has to be updated to the latest version, because the lowest installed 2.0.4 is in conflict with
 # one of the Apigee PHP SDK's required library's (symfony/property-info:^3.2) minimum requirement.
@@ -17,11 +56,10 @@ composer require ${DEPENDENCIES} "drupal/${DRUPAL_MODULE_NAME}"
 # Install this to get more detailed output from PHPUnit.
 composer require ${DEPENDENCIES} limedeck/phpunit-detailed-printer:^3.2.0
 composer show
-# Make sure that the log folder is writable for www-data.
-sudo -u root chown www-data:wodby /mnt/files/log
 # Download the test runner
-curl -L -o testrunner https://github.com/Pronovix/testrunner/releases/download/v0.2/testrunner-linux-amd64
-chmod +x ./testrunner
+curl -L -o /var/www/html/testrunner https://github.com/Pronovix/testrunner/releases/download/v0.2/testrunner-linux-amd64
+chmod +x /var/www/html/testrunner
 # Do not exit if any phpunit tests fail, we still want to see the performance
 # information.
-sudo -u root -E sudo -u www-data -E ./testrunner -threads=$THREADS -root=./modules/contrib/apigee_edge/tests -command="./vendor/bin/phpunit -c core -v --debug"
+set +e
+sudo -u root -E sudo -u www-data -E /var/www/html/testrunner -threads=$THREADS -root=./web/modules/contrib/apigee_edge/tests -command="./web/vendor/bin/phpunit -c web/core -v --debug --printer \Drupal\Tests\Listeners\HtmlOutputPrinter"
