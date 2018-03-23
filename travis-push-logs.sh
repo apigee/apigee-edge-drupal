3dce985a9ac3541abb463f79f8d75a5f2f118dd3
Dezső BICZÓ <mxr576@gmail.com>
2018-03-23 17:34:25 +0100

    Push logs about failed builds to an another repository for easier debugging.

---
+++ travis-push-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Because bash is missing from any defined path in PATH (like /usr/bin).
+# #!/usr/bin/env bash
+
+set -e
+
+# Initial GIT setup.
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+# Copy logs from the PHP container.
+docker cp my_project_php:/mnt/files/log .
+cd log
+# Commit and push logs to the git repo.
+git init
+git checkout -b ${TRAVIS_BUILD_NUMBER}
+git add .
+git commit -am "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin https://${LOGS_REPO_USER}:${LOGS_REPO_PASSWORD}@${LOGS_REPO_HOST}/${LOGS_REPO_USER}/${LOGS_REPO_NAME}.git
+git push -u origin ${TRAVIS_BUILD_NUMBER}
