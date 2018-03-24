89a706033be5abc238ff3e9ff1afa1843be88ca4
Dezső BICZÓ <mxr576@gmail.com>
2018-03-24 10:08:23 +0100

    Generate unique branch name for each builds. (Jobs can be restarted on Travis.)

--- travis-push-logs.sh
+++ travis-push-logs.sh
@@ -12,8 +12,9 @@ docker cp my_project_php:/mnt/files/log .
 cd log
 # Commit and push logs to the git repo.
 git init
-git checkout -b ${TRAVIS_JOB_NUMBER}
+BRANCH_NAME=${TRAVIS_JOB_NUMBER}-$(date +"%y%m%d-%H%M")
+git checkout -b ${BRANCH_NAME}
 git add .
-git commit -am "Travis build: $TRAVIS_JOB_NUMBER"
+git commit -am "Travis build: ${TRAVIS_JOB_NUMBER}"
 git remote add origin https://${LOGS_REPO_USER}:${LOGS_REPO_PASSWORD}@${LOGS_REPO_HOST}/${LOGS_REPO_USER}/${LOGS_REPO_NAME}.git
-git push -u origin ${TRAVIS_JOB_NUMBER}
+git push -u origin ${BRANCH_NAME}
