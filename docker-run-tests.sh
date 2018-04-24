78a63b51805a8aa4518742b4eafbefe0aef473c1
Dezső BICZÓ <mxr576@gmail.com>
2018-04-24 17:29:51 +0200

    Let module to describe its exact dev and non-dev dependencies (including patches) just like a normal PHP library.

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -4,38 +4,42 @@
 
 set -e
 
-SITE_ROOT="/var/www/html/web"
+MODULE_PATH="/opt/drupal-module"
+WEB_ROOT="/var/www/html/build"
+WEB_ROOT_PARENT="/var/www/html"
+COMPOSER_OPTIONS="--no-interaction --prefer-dist"
 
 # We mounted the cache/files folder from the host so we have to fix permissions
 # on the parent cache folder because it did not exist before.
 sudo -u root sh -c "chown -R wodby:wodby /home/wodby/.composer/cache"
 
-# Ensure that we are in the correct directory.
-mkdir -p ${SITE_ROOT} && cd ${SITE_ROOT}
+# Copy module's source to web root parent.
+cp -R ${MODULE_PATH}/. ${WEB_ROOT_PARENT} && cd ${WEB_ROOT_PARENT}
 
-COMPOSER_OPTIONS="$SITE_ROOT --no-interaction --prefer-dist"
+# Install module with its dependencies (including dev dependencies).
+composer update ${COMPOSER_OPTIONS} ${DEPENDENCIES} -o --with-dependencies
 
-# We are using drupal-composer/drupal-project instead of drupal/drupal because we would like to update all
-# libraries, including Drupal, to the latest version when doing "highest" testing.
-if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then \
-  composer create-project drupal/drupal:${DRUPAL_CORE} ${COMPOSER_OPTIONS}; else \
-  composer create-project drupal/drupal:^${DRUPAL_CORE} ${COMPOSER_OPTIONS} && composer update -o --with-dependencies; \
-  fi
+# Symlink module to the contrib folder.
+ln -s ${MODULE_PATH} ${WEB_ROOT}/modules/contrib/${DRUPAL_MODULE_NAME}
 
 # Based on https://www.drupal.org/node/244924.
 # Also fix permissions on directory and .htaccess file.
-sudo -u root sh -c "chown -R wodby:www-data . \
-    && find . -type d -exec chmod 6750 '{}' \; \
-    && find . -type f -exec chmod 0640 '{}' \; \
-    && chmod 755 . \
-    && chmod 644 .htaccess"
+sudo -u root sh -c "chown -R wodby:www-data $WEB_ROOT \
+    && find $WEB_ROOT -type d -exec chmod 6750 '{}' \; \
+    && find $WEB_ROOT -type f -exec chmod 0640 '{}' \; \
+    && chmod 755 $WEB_ROOT \
+    && chmod 644 $WEB_ROOT/.htaccess"
 
-sudo -u root sh -c "mkdir -p $SITE_ROOT/sites/default/files \
-    && chown -R wodby:www-data $SITE_ROOT/sites/default/files \
-    && chmod 6770 $SITE_ROOT/sites/default/files"
+sudo -u root sh -c "mkdir -p $WEB_ROOT/sites/default/files \
+    && chown -R wodby:www-data $WEB_ROOT/sites/default/files \
+    && chmod 6770 $WEB_ROOT/sites/default/files"
 
-## Pre-create simpletest directory.
-sudo -u root mkdir -p ${SITE_ROOT}/sites/simpletest
+# Pre-create simpletest directory...
+sudo -u root mkdir -p ${WEB_ROOT}/sites/simpletest
+# and another required libraries.
+# (These are required by core/phpunit.xml.dist).
+sudo -u root mkdir -p ${WEB_ROOT}/profiles
+sudo -u root mkdir -p ${WEB_ROOT}/themes
 
 # Make sure that the log folder is writable for both www-data and wodby users.
 # Also create a dedicated folder for PHPUnit outputs.
@@ -48,38 +52,18 @@ sudo -u root sh -c "chown -R www-data:wodby /mnt/files/log \
 # Change location of the browser_output folder, because it seems even if
 # BROWSERTEST_OUTPUT_DIRECTORY is set the html output is printed out to
 # https://github.com/drupal/core/blob/8.5.0/tests/Drupal/Tests/BrowserTestBase.php#L1086
-sudo -u root ln -s /mnt/files/log/simpletest/browser_output ${SITE_ROOT}/sites/simpletest/browser_output
+sudo -u root ln -s /mnt/files/log/simpletest/browser_output ${WEB_ROOT}/sites/simpletest/browser_output
 
-# Fix permissions on on simpletest and its subfolders.
-sudo -u root sh -c "chown -R www-data:wodby $SITE_ROOT/sites/simpletest \
-    && chmod -R 6750 $SITE_ROOT/sites/simpletest"
+# Fix permissions on on simpletest and its sub-folders.
+sudo -u root sh -c "chown -R www-data:wodby $WEB_ROOT/sites/simpletest \
+    && chmod -R 6750 $WEB_ROOT/sites/simpletest"
 
-# This library has to be updated to the latest version, because the lowest installed 2.0.4 is in conflict with
-# one of the Apigee PHP SDK's required library's (symfony/property-info:^3.2) minimum requirement.
-if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then composer require phpdocumentor/reflection-docblock:3.1.0; fi;
-# Do not allow to install the latest PHPUnit version on lowest testing.
-# We have to install PHPUnit >= 6.5 instead of >=6.1 here until this
-# Drupal core issue has not been fixed:
-# https://www.drupal.org/project/drupal/issues/2947888
-if [[ "$DEPENDENCIES" = --prefer-lowest ]]; then jq '. + { "conflict": { "phpunit/phpunit": ">6.5.0" }}' composer.json > tmp.json && mv tmp.json composer.json; fi;
-composer drupal-phpunit-upgrade
-# Fix failing tests caused by the recent changes in the behat/mink library.
-# https://github.com/minkphp/Mink/pull/760
-# https://www.drupal.org/project/drupal/issues/2956279
-composer require --dev behat/mink:dev-master#9ea1cebe
-# Install a required patch to fix failing tests.
-# "Time Ago summary does not render on Manage Display for Datetime"
-# http://dgo.to/2686409
-wget https://www.drupal.org/files/issues/2686409-45.patch
-git apply -v < 2686409-45.patch
-composer config repositories.library path /opt/drupal-module
-composer require ${DEPENDENCIES} "drupal/${DRUPAL_MODULE_NAME}"
-# Install this to get more detailed output from PHPUnit.
-composer require ${DEPENDENCIES} limedeck/phpunit-detailed-printer:^3.2.0
+# Let's see installed dependencies.
 composer show
-# Download the test runner
+
+# Download the test runner.
 curl -L -o /var/www/html/testrunner https://github.com/Pronovix/testrunner/releases/download/v0.4/testrunner-linux-amd64
 chmod +x /var/www/html/testrunner
 # Do not exit if any PHPUnit test fails.
 set +e
-sudo -u root -E sudo -u www-data -E /var/www/html/testrunner -verbose -threads=$THREADS -root=${SITE_ROOT}/modules/contrib/apigee_edge/tests -command="$SITE_ROOT/vendor/bin/phpunit -c $SITE_ROOT/core -v --debug --printer \Drupal\Tests\Listeners\HtmlOutputPrinter"
+sudo -u root -E sudo -u www-data -E /var/www/html/testrunner -verbose -threads=$THREADS -root=${WEB_ROOT}/modules/contrib/apigee_edge/tests -command="WEB_ROOT_PARENT/vendor/bin/phpunit -c $WEB_ROOT/core -v --debug --printer \Drupal\Tests\Listeners\HtmlOutputPrinter"
