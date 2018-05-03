5241843d971c110bef8ac703a52ffda6ab89e71f
peterserfozo <peter.serfozo@pronovix.com>
2018-05-03 23:45:00 +0200

    Added OAuth key type interface.

---
+++ src/Plugin/EdgeOAuthKeyTypeInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_edge\Plugin;
+
+use Drupal\key\KeyInterface;
+
+/**
+ * Defines an interface for Apigee Edge OAuth Key Type plugins.
+ */
+interface EdgeOAuthKeyTypeInterface extends EdgeKeyTypeInterface {
+
+  /**
+   * Gets the authorization server.
+   *
+   * @param \Drupal\key\KeyInterface $key
+   *   The key entity.
+   *
+   * @return string
+   *   The authorization server.
+   */
+  public function getAuthorizationServer(KeyInterface $key) : string;
+
+  /**
+   * Gets the client ID.
+   *
+   * @param \Drupal\key\KeyInterface $key
+   *   The key entity.
+   *
+   * @return string
+   *   The client ID.
+   */
+  public function getClientId(KeyInterface $key) : string;
+
+  /**
+   * Gets the client secret.
+   *
+   * @param \Drupal\key\KeyInterface $key
+   *   The key entity.
+   *
+   * @return string
+   *   The client secret.
+   */
+  public function getClientSecret(KeyInterface $key) : string;
+
+}
