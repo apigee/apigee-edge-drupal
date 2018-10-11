2f3a0ee6dae13bd8e4485bbfd250ab5b313a2a0a
Dezső BICZÓ <mxr576@gmail.com>
2018-10-11 08:29:34 +0200

    Do not try to load authentication key object with empty (null) id.

---
+++ src/Exception/AuthenticationKeyException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\apigee_edge\Exception;
+
+use Apigee\Edge\Exception\ApiException;
+
+/**
+ * Defines an exception for authentication key problems.
+ */
+class AuthenticationKeyException extends ApiException {
+
+}
--- src/Exception/KeyValueMalformedException.php
+++ src/Exception/KeyValueMalformedException.php
@@ -8,20 +8,20 @@ use Throwable;
 /**
  * Defines an exception for when a key value is malformed.
  */
-class KeyValueMalformedException extends ApiException {
+class KeyValueMalformedException extends AuthenticationKeyException {
 
   /**
-   * The key value field that cannot be read.
+   * The key value field that is malformed.
    *
    * @var string
    */
-  protected $missingField;
+  protected $problematicField;
 
   /**
    * KeyValueMalformedException constructor.
    *
-   * @param string $missing_field
-   *   The missing key value field.
+   * @param string $problematic_field
+   *   Name of the field that caused the issue.
    * @param string $message
    *   Exception message.
    * @param int $code
@@ -29,10 +29,18 @@ class KeyValueMalformedException extends ApiException {
    * @param \Throwable|null $previous
    *   Previous exception.
    */
-  public function __construct($missing_field = NULL, $message = '', $code = 0, Throwable $previous = NULL) {
-    $this->missingField = $missing_field;
+  public function __construct($problematic_field, $message = '', $code = 0, Throwable $previous = NULL) {
+    $this->problematicField = $problematic_field;
+    // We do not expose the name of problematic field by default in the message.
     $message = $message ?: t('Apigee Edge API authentication key is malformed or not readable.');
     parent::__construct($message, $code, $previous);
+  }
+
+  /**
+   * @return string
+   */
+  public function getProblematicField(): string {
+    return $this->problematicField;
   }
 
 }
