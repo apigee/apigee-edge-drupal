9bc6b07ce6f5ab10443fd2e19615a27b52684670
Dezső BICZÓ <mxr576@gmail.com>
2018-10-16 12:24:51 +0200

    Do not block existing Drupal user if its developer has been block in Apigee Edge.

--- src/Job/UserUpdate.php
+++ src/Job/UserUpdate.php
@@ -32,6 +32,15 @@ class UserUpdate extends UserCreateUpdate {
    */
   protected function beforeUserSave(DeveloperToUserConversionResult $result): void {
     parent::beforeUserSave($result);
+
+    /** @var \Drupal\user\UserInterface $original_user */
+    $original_user = \Drupal::entityTypeManager()->getStorage('user')->loadUnchanged($result->getUser()->id());
+    // Even if the developer has been blocked in Apigee Edge we should not block
+    // its Drupal user automatically when syncing.
+    if ($original_user->isActive() && $result->getUser()->isBlocked()) {
+      $result->getUser()->activate();
+    }
+
     $context = [
       'link' => $result->getUser()->toLink(t('View user'))->toString(),
     ];
@@ -41,7 +50,6 @@ class UserUpdate extends UserCreateUpdate {
     // contained an incorrect field value.
     if (count($result->getProblems()) > 0) {
       /** @var \Drupal\user\UserInterface $original_user */
-      $original_user = user_load_by_mail($this->email);
       foreach ($result->getProblems() as $problem) {
         // Do not apply rollback on base fields.
         if ($problem instanceof DeveloperToUserConversationInvalidValueException && !in_array($problem->getTarget(), $this->userDeveloperConverter()::DEVELOPER_PROP_USER_BASE_FIELD_MAP)) {
