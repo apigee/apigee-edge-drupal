a3a24c9d15ab1dd47b5fdc3ba03f3956a555f566
Dezső BICZÓ <mxr576@gmail.com>
2018-11-16 15:13:54 +0100

    New developer entity.

--- src/Entity/DeveloperInterface.php
+++ src/Entity/DeveloperInterface.php
@@ -3,18 +3,19 @@
 /**
  * Copyright 2018 Google Inc.
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 2 as published by the
- * Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
- * License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
 
 namespace Drupal\apigee_edge\Entity;
@@ -23,17 +24,14 @@ use Apigee\Edge\Api\Management\Entity\DeveloperInterface as EdgeDeveloperInterfa
 use Drupal\user\EntityOwnerInterface;
 
 /**
- * Defines an interface for developer entity objects.
+ * Defines an interface for develper entity objects.
  */
 interface DeveloperInterface extends EdgeDeveloperInterface, EdgeEntityInterface, EntityOwnerInterface {
 
   /**
-   * Sets the original email address of the developer.
-   *
-   * @param null|string $originalEmail
-   *   The original email address.
+   * Resets the original email.
    */
-  public function setOriginalEmail(string $originalEmail);
+  public function resetOriginalEmail() : void;
 
   /**
    * Set status of the developer.
@@ -42,5 +40,13 @@ interface DeveloperInterface extends EdgeDeveloperInterface, EdgeEntityInterface
    *   Status of the entity.
    */
   public function setStatus(string $status): void;
+
+  /**
+   * Returns the original (unmodified) email address of a developer.
+   *
+   * @return null|string
+   *   The original (unmodified) email address of a developer.
+   */
+  public function getOriginalEmail(): ?string;
 
 }
