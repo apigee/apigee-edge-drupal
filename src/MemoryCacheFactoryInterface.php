7c4bd0667223c298ba39d5cecbc993872774fe68
Dezső BICZÓ <mxr576@gmail.com>
2018-11-29 17:16:58 +0100

    Introducing new memory cache factory service.

---
+++ src/MemoryCacheFactoryInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_edge;
+
+use Drupal\Core\Cache\MemoryCache\MemoryCacheInterface;
+
+/**
+ * Base definition of the memory cache factory service.
+ */
+interface MemoryCacheFactoryInterface {
+
+  /**
+   * Gets a memory cache backend class for a given cache bin.
+   *
+   * @param string $bin
+   *   The cache bin for which a cache backend object should be returned.
+   *
+   * @return \Drupal\Core\Cache\MemoryCache\MemoryCacheInterface
+   *   The memory cache backend object associated with the specified bin.
+   */
+  public function get($bin): MemoryCacheInterface;
+
+}
