8fdf7a46d691269c9f57a01fb3f097a80de1ce0c
Dezső BICZÓ <mxr576@gmail.com>
2018-12-10 13:57:18 +0100

    Introducing new view builders for (developer) apps.
    This commit deprecates the DeveloperAppCallbackUrlCheckTrait trait. Now,
    callback validation errors gets displayed in the entity view instead
    outside of the view (on the top of the page).

--- src/Controller/DeveloperAppViewControllerForDeveloper.php
+++ src/Controller/DeveloperAppViewControllerForDeveloper.php
@@ -19,18 +19,47 @@
 
 namespace Drupal\apigee_edge\Controller;
 
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\apigee_edge\Entity\DeveloperAppInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Displays the view page of a developer app for a given user on the UI.
  */
-class DeveloperAppViewControllerForDeveloper extends DeveloperAppViewController {
+class DeveloperAppViewControllerForDeveloper extends ControllerBase {
+
+  /**
+   * The entity type manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * DeveloperAppViewControllerForDeveloper constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function view(EntityInterface $app, $view_mode = 'full') {
-    $build = parent::view($app, $view_mode);
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function view(DeveloperAppInterface $app) {
+    $build = $this->entityTypeManager->getViewBuilder($app->getEntityTypeId())->view($app);
     return $build;
   }
 
