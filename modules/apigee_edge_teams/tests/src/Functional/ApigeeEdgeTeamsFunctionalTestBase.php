a9eda7e3b20c200b2c707608d1c2e8ac3ac87b89
Péter Serfőző <peter.serfozo@pronovix.com>
2019-01-13 19:42:35 +0100

    Added base class to teams functional tests.

---
+++ modules/apigee_edge_teams/tests/src/Functional/ApigeeEdgeTeamsFunctionalTestBase.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Drupal\Tests\apigee_edge_teams\Functional;
+
+use Drupal\Tests\apigee_edge\Functional\ApigeeEdgeFunctionalTestBase;
+
+/**
+ * Base class for Apigee Edge Teams functional tests.
+ */
+abstract class ApigeeEdgeTeamsFunctionalTestBase extends ApigeeEdgeFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'apigee_edge_teams',
+  ];
+
+}
