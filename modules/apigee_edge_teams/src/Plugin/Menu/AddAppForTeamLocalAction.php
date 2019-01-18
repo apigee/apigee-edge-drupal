2e2fb361f524564b19842d4751afb9048f3accaa
Dezső BICZÓ <mxr576@gmail.com>
2019-01-18 08:29:21 +0100

    Use lowercase label for consistency reasons.

--- modules/apigee_edge_teams/src/Plugin/Menu/AddAppForTeamLocalAction.php
+++ modules/apigee_edge_teams/src/Plugin/Menu/AddAppForTeamLocalAction.php
@@ -82,7 +82,7 @@ class AddAppForTeamLocalAction extends LocalActionDefault {
    * {@inheritdoc}
    */
   public function getTitle(Request $request = NULL) {
-    return $this->t('Add @app', ['@app' => $this->teamAppEntity->getSingularLabel()]);
+    return $this->t('Add @app', ['@app' => $this->teamAppEntity->getLowercaseLabel()]);
   }
 
 }
