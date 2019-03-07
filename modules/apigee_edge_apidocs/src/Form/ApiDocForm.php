2b7a7157e42825b79fd81dbf43b577e8905bf06e
Arlina Espinoza <arlina.espinoza@gmail.com>
2019-03-07 00:31:08 -0800

    Correctly show the entity label when the API Doc entity gets saved.

--- modules/apigee_edge_apidocs/src/Form/ApiDocForm.php
+++ modules/apigee_edge_apidocs/src/Form/ApiDocForm.php
@@ -51,7 +51,7 @@ class ApiDocForm extends ContentEntityForm {
       ]));
     }
     else {
-      $this->messenger()->addMessage($this->t('Saved the $label @entity_type_label.', [
+      $this->messenger()->addMessage($this->t('Saved the %label @entity_type_label.', [
         '%label' => $entity->label(),
         '@entity_type_label' => $singular_label,
       ]));
