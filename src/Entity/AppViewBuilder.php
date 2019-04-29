d073cbdfaa2a6ce1bb0cd4626c01f360bc19973b
Dezső BICZÓ <mxr576@gmail.com>
2019-04-29 15:59:27 +0200

    The render array can contain control words

--- src/Entity/AppViewBuilder.php
+++ src/Entity/AppViewBuilder.php
@@ -21,6 +21,7 @@
 namespace Drupal\apigee_edge\Entity;
 
 use Drupal\Core\Entity\EntityViewBuilder;
+use Drupal\Core\Render\Element;
 use Drupal\Core\Url;
 
 /**
@@ -33,31 +34,28 @@ class AppViewBuilder extends EntityViewBuilder {
    */
   public function buildMultiple(array $build_list) {
     $results = parent::buildMultiple($build_list);
-    foreach ($results as $id => $result) {
+    foreach (Element::children($results) as $key) {
       /** @var \Drupal\apigee_edge\Entity\AppInterface $app */
-      $app = $result["#{$this->entityTypeId}"];
-      // The callback field is visible.
-      if (array_key_exists('callbackUrl', $result)) {
-        // Display an error message if the callback url field value does not
-        // contain a valid URI.
-        if (!empty($app->getCallbackUrl()) && $app->getCallbackUrl() !== $app->get('callbackUrl')->value) {
-          try {
-            Url::fromUri($app->getCallbackUrl());
-          }
-          catch (\Exception $exception) {
-            $results[$id]['callback_url_error'] = [
-              '#theme' => 'status_messages',
-              '#message_list' => [
-                'warning' => [$this->t('The @field value should be fixed. @message', [
-                  '@field' => $app->getFieldDefinition('callbackUrl')->getLabel(),
-                  '@message' => $exception->getMessage(),
-                ]),
-                ],
+      $app = $results[$key]["#{$this->entityTypeId}"];
+      // If the callback field is visible, display an error message if the
+      // callback url field value does not contain a valid URI.
+      if (array_key_exists('callbackUrl', $results[$key]) && !empty($app->getCallbackUrl()) && $app->getCallbackUrl() !== $app->get('callbackUrl')->value) {
+        try {
+          Url::fromUri($app->getCallbackUrl());
+        }
+        catch (\Exception $exception) {
+          $results[$key]['callback_url_error'] = [
+            '#theme' => 'status_messages',
+            '#message_list' => [
+              'warning' => [$this->t('The @field value should be fixed. @message', [
+                '@field' => $app->getFieldDefinition('callbackUrl')->getLabel(),
+                '@message' => $exception->getMessage(),
+              ]),
               ],
+            ],
               // Display it on the top of the view.
-              '#weight' => -100,
-            ];
-          }
+            '#weight' => -100,
+          ];
         }
       }
     }
