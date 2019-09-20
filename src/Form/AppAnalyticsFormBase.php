20f66ad8ded24ee746990767fdfbb2aef6b191cc
Arlina Espinoza <arlina.espinoza@gmail.com>
2019-09-20 10:40:29 -0700

    [#262] Fix app analytics submit redirect.

--- src/Form/AppAnalyticsFormBase.php
+++ src/Form/AppAnalyticsFormBase.php
@@ -445,14 +445,16 @@ abstract class AppAnalyticsFormBase extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $url = $this->urlGenerator->generateFromRoute('<current>', [], [
+    $route = \Drupal::routeMatch();
+    $parameters = $route->getRawParameters()->all();
+    $options = [
       'query' => [
         'metric' => $form_state->getValue('metrics'),
         'since' => $form_state->getValue('since')->getTimeStamp(),
         'until' => $form_state->getValue('until')->getTimeStamp(),
       ],
-    ]);
-    $form_state->setRedirectUrl(Url::fromUri('internal:' . $url));
+    ];
+    $form_state->setRedirect($route->getRouteName(), $parameters, $options);
   }
 
   /**
