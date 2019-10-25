433320355c30d0659cb2f74fa72b03f6c24ee4dc
Dezső BICZÓ <mxr576@gmail.com>
2019-10-25 08:52:16 +0200

    Fix access to the user/[uid]/create-app form
    Closes #285

--- src/Access/AppsPageAccessCheck.php
+++ src/Access/AppsPageAccessCheck.php
@@ -53,4 +53,27 @@ class AppsPageAccessCheck implements AccessInterface {
     );
   }
 
+  /**
+   * Grant access to the user/[uid]/create-apps page.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The parametrized route.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The currently logged in account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function createAppAccess(RouteMatchInterface $route_match, AccountInterface $account) {
+    /** @var \Drupal\user\UserInterface $user */
+    $user = $route_match->getParameter('user');
+    if ($user === NULL) {
+      return AccessResult::forbidden('User is missing from route.');
+    }
+    return AccessResultAllowed::allowedIf(
+      ($account->id() === $user->id() && $account->hasPermission('create developer_app')) ||
+      ($account->hasPermission('administer developer_app'))
+    );
+  }
+
 }
