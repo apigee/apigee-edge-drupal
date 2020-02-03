4020919d08122aaff496036523abef0c8c2aa7d2
Arlina Espinoza <arlina.espinoza@gmail.com>
2020-02-03 10:45:28 -0800

    Adding template for circle-cI.

---
+++ tests/src/Behat/features/bootstrap/ApigeeEdgeFeatureContext.php
@@ -0,0 +1,33 @@
+<?php
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Behat steps for testing the apigee_edge module.
+ *
+ * @codingStandardsIgnoreStart
+ */
+class ApigeeEdgeFeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  /**
+   * Setup for the test suite, enable some required modules and add content
+   * title.
+   *
+   * @BeforeSuite
+   */
+  public static function prepare(BeforeSuiteScope $scope) {
+    /** @var \Drupal\Core\Extension\ModuleHandler $moduleHandler */
+    $moduleHandler = \Drupal::service('module_handler');
+    if (!$moduleHandler->moduleExists('apigee_edge')) {
+      \Drupal::service('module_installer')->install(['apigee_edge']);
+    }
+
+    // Also uninstall the inline form errors module for easier testing.
+    if ($moduleHandler->moduleExists('inline_form_errors')) {
+      \Drupal::service('module_installer')->uninstall(['inline_form_errors']);
+    }
+  }
+
+}
