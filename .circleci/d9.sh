864c0052638b26285d65aef33e8e3d352c1a4a2a
Arlina Espinoza <arlina.espinoza@gmail.com>
2020-05-12 00:41:16 -0700

    [#398] Add Drupal 9 readiness checks to CI - updates.

--- .circleci/d9.sh
+++ .circleci/d9.sh
@@ -7,6 +7,3 @@ fi
 
 vendor/bin/drupal-check --no-progress --memory-limit=1000M --format=junit $1 > /var/www/html/artifacts/d9/d9check.xml
 
-# to-do: Remove when PR gets merged into mglaman/drupal-check:
-# https://github.com/mglaman/drupal-check/pull/155
-sed -i '/%/d' /var/www/html/artifacts/d9/d9check.xml
