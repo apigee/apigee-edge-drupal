6f53db8371dc9995cafa809908bba8e81e009516
zakiya <zakiya@chapterthree.com>
2020-11-23 20:53:54 -0800

    Better variable names and code sniffer fixes.

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -1,40 +1,83 @@
 <?php
 
 namespace Drupal\apigee_edge\Controller;
+
 use Drupal\Core\Entity\EntityBase;
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+/**
+ * Controller to redirect changed paths.
+ */
 class RedirectController extends ControllerBase {
 
+  /**
+   * The entity id from path input.
+   *
+   * @var string
+   */
   private $entity_id;
+
+  /**
+   * Entity type from path input.
+   *
+   * @var string
+   */
   private $entity_type_id;
 
-  private function setEntityID($entity = null) {
-    if (is_string($entity)) {
-      $this->entity_id = $entity;
-    } elseif ($entity instanceof EntityBase ) {
-      $this->entity_id = $entity->id();
+  /**
+   * Identity the entity id.
+   *
+   * @param mixed $entity_slug
+   *   Convert entity slug from path to id.
+   */
+  private function setEntityId($entity_slug) {
+    if (is_string($entity_slug)) {
+      $this->entity_id = $entity_slug;
+    }
+
+    elseif ($entity_slug instanceof EntityBase) {
+      $this->entity_id = $entity_slug->id();
+    }
+
+  }
+
+  /**
+   * Identity the type of entity.
+   *
+   * @param mixed $entity_slug
+   *   Entity slug from input.
+   */
+  private function setEntityTypeId($entity_slug) {
+    if ($entity_slug instanceof EntityBase) {
+      $this->entity_type_id = $entity_slug->getEntityTypeId();
     }
   }
 
-  private function setEntityTypeID($entity = null) {
-    if ($entity instanceof EntityBase ) {
-      $this->entity_type_id = $entity->getEntityTypeId();
+  /**
+   * Redirect the given path to the correct route.
+   *
+   * @param mixed $entity_slug
+   *   Entity slug from input.
+   * @param string $redirect_route
+   *   Route parameter defined as defined in routing.yml.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   Return a 404.
+   */
+  public function manageRedirect($entity_slug = NULL, $redirect_route = 'system.404') {
+
+    if ($entity_slug != NULL) {
+      $this->setEntityId($entity_slug);
+      $this->setEntityTypeId($entity_slug);
+      $redirect = $this->redirect($redirect_route, [$this->entity_type_id => $this->entity_id]);
     }
-  }
 
-  public function manageRedirect($entity = null, $route = 'system.404') {
-
-    if ($entity !=null) {
-      $this->setEntityID($entity);
-      $this->setEntityTypeId($entity);
-      $redirect = $this->redirect($route, [$this->entity_type_id => $this->entity_id]);
-    } else {
-      $redirect = $this->redirect($route);
+    else {
+      $redirect = $this->redirect($redirect_route);
     }
 
     return $redirect;
 
   }
+
 }
