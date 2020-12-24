a28959472595391f92ed4e292975b294d502179d
shishir <shishir.suvarna@intelliswift.com>
2020-12-24 18:33:27 +0530

    Field_ui dependency removed from Apigee edge module

--- modules/apigee_edge_teams/tests/src/Functional/UiTest.php
+++ modules/apigee_edge_teams/tests/src/Functional/UiTest.php
@@ -34,13 +34,14 @@ class UiTest extends ApigeeEdgeTeamsFunctionalTestBase {
 
   use EntityUtilsTrait;
   use FieldUiTestTrait;
- 
+
   /**
    * {@inheritdoc}
    */
   protected static $modules = [
     'field_ui',
   ];
+
   /**
    * The team entity storage.
    *
--- tests/src/Functional/DeveloperAppUITest.php
+++ tests/src/Functional/DeveloperAppUITest.php
@@ -37,7 +37,12 @@ class DeveloperAppUITest extends ApigeeEdgeFunctionalTestBase {
   use EntityUtilsTrait;
 
   protected const DUPLICATE_MACHINE_NAME = 'The machine-readable name is already in use. It must be unique.';
-
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'field_ui',
+  ];
   /**
    * {@inheritdoc}
    */
