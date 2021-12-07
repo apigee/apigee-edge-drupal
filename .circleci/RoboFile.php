2a22cb20c1fa9a9a100c345f74ded1480a2bd0ee
Hirenkumar Parmar <hirenkumar@google.com>
2021-12-07 13:27:20 +0530

    Update dependency of rules to 8.x-3.0-alpha7

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -488,9 +488,7 @@ class RoboFile extends \Robo\Tasks
         $config->require->{"drupal/core-dev"} = '^8.9';
 
         // Add rules for testing apigee_edge_actions (only for D8).
-        $config->require->{"drupal/rules"} = "3.0.0-alpha6";
-        // Add typed_data beta1 as dev version of typed_data is not compatible with D8.
-        $config->require->{"drupal/typed_data"} = "1.0.0-beta1 as 1.x-dev";
+        $config->require->{"drupal/rules"} = "3.0.0-alpha7";
 
         // We require Drupal drush and console for some tests.
         $config->require->{"drupal/console"} = "~1.0";
