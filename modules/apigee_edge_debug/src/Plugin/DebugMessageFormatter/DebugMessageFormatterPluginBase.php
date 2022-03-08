6057a42780db5ae732a18bc8f00e76fe44bf266a
Shishir <75600200+shishir-intelli@users.noreply.github.com>
2022-03-08 22:37:39 +0530

    Replaced deprecated "Formatter::formatResponse()" with "formatResponseForRequest" method (#668)

--- modules/apigee_edge_debug/src/Plugin/DebugMessageFormatter/DebugMessageFormatterPluginBase.php
+++ modules/apigee_edge_debug/src/Plugin/DebugMessageFormatter/DebugMessageFormatterPluginBase.php
@@ -155,7 +155,7 @@ abstract class DebugMessageFormatterPluginBase extends PluginBase implements Con
         }
       }
     }
-    return $this->getFormatter()->formatResponse($response);
+    return $this->getFormatter()->formatResponseForRequest($response, $request);
   }
 
   /**
