0de577db0fb76d93c1a4a301bd959786602a7554
David Coote <david.coote@terem.com.au>
2022-03-29 14:41:29 +1030

    Fix codesniffer errors

--- modules/apigee_edge_teams/src/Entity/Team.php
+++ modules/apigee_edge_teams/src/Entity/Team.php
@@ -348,12 +348,12 @@ class Team extends AttributesAwareFieldableEdgeEntityBase implements TeamInterfa
   }
 
   /**
-   * @return string|null
+   * This is required by jsonapi.
    *
    * This is here because the jsonapi module relies on a uuid to build end points and breaks if one is not present.
    * Creating a real UUID for this entity type is not possible as entity data is stored on Apigee.
    */
-  public function uuid() {
+  public function uuid(): string {
     return $this->id();
   }
 
