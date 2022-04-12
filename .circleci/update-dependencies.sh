30b82a42faf1b82ea9e770cb872e0ae0fa9c98ec
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2022-04-12 17:14:20 +0530

    Fix for error in action debug where incorrect class is passed (#688)
    * Fix for error in action debug where incorrect class is passed

--- .circleci/update-dependencies.sh
+++ .circleci/update-dependencies.sh
@@ -9,7 +9,6 @@ robo add:dependencies-from modules/$1/composer.json
 robo drupal:version $2
 robo configure:module-dependencies
 robo update:dependencies
-robo do:extra $2
 
 # Touch a flag so we know dependencies have been set. Otherwise, there is no
 # easy way to know this step needs to be done when running circleci locally since
