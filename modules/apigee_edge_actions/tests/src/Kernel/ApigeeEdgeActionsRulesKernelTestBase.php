463c3333ca1d1d7806e3ad435d544cfd434fcce2
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2022-06-22 11:48:52 +0530

    Fix for deprecation notice:$modules property must be declared protected (#722)

--- modules/apigee_edge_actions/tests/src/Kernel/ApigeeEdgeActionsRulesKernelTestBase.php
+++ modules/apigee_edge_actions/tests/src/Kernel/ApigeeEdgeActionsRulesKernelTestBase.php
@@ -39,7 +39,7 @@ class ApigeeEdgeActionsRulesKernelTestBase extends RulesKernelTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = [
+  protected static $modules = [
     'apigee_edge_actions',
     'apigee_edge_actions_debug',
     'apigee_edge',
