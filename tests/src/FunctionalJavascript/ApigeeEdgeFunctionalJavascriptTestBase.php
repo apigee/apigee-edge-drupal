647289b07a8878bebd6d3ac6182625551ccb2cbd
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2023-11-15 21:42:38 +0530

    Fix for deprecation Connection::tablePrefix() is deprecated (#979)

--- tests/src/FunctionalJavascript/ApigeeEdgeFunctionalJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ApigeeEdgeFunctionalJavascriptTestBase.php
@@ -64,9 +64,7 @@ abstract class ApigeeEdgeFunctionalJavascriptTestBase extends WebDriverTestBase 
     }
     /** @var \Drupal\Core\Database\Connection $database */
     $database = $this->container->get('database');
-    // @todo tablePrefix() is deprecated in Drupal 10.1
-    // @phpstan-ignore-next-line
-    $test_id = str_replace('test', '', $database->tablePrefix());
+    $test_id = str_replace('test', '', $database->getPrefix());
     // Add table suffix (test id) to the file name and ensure the generated
     // file name is unique.
     $filename = \Drupal::service('file_system')
