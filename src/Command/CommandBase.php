e5a38d5cb6ecb4f281e036772f1f7e26e2152070
Divyajose <75604843+divya-intelli@users.noreply.github.com>
2023-11-24 14:03:45 +0530

    Deprecate classes that is dependent on drupal/console (#989)
    * Deprecated classes that is dependent on drupal/console

--- src/Command/CommandBase.php
+++ src/Command/CommandBase.php
@@ -19,6 +19,8 @@
 
 namespace Drupal\apigee_edge\Command;
 
+@trigger_error('The ' . __NAMESPACE__ . '\CommandBase is deprecated in apigee_edge:3.0.4 and is removed from apigee_edge:3.1.0. Drupal/console is not compatible with Drupal 10. See https://github.com/apigee/apigee-edge-drupal/issues/984', E_USER_DEPRECATED);
+
 use Drupal\apigee_edge\CliServiceInterface;
 use Drupal\Console\Core\Command\Command;
 use Drupal\Console\Core\Command\Shared\CommandTrait;
@@ -32,6 +34,11 @@ use Symfony\Component\Console\Style\StyleInterface;
 
 /**
  * Class CommandBase for shared functionality.
+ *
+ * @deprecated in apigee_edge:3.0.4 and is removed from apigee_edge:3.1.0.
+ *  Drupal/console is not compatible with Drupal 10.
+ *
+ * @see https://github.com/apigee/apigee-edge-drupal/issues/984
  */
 abstract class CommandBase extends Command {
 
