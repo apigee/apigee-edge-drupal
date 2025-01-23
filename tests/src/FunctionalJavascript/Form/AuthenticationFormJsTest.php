94395a52080427f492d694f64ebb1529b071693c
Kedar Khaire <kedark@google.com>
2025-01-23 13:45:08 +0530

    Default theme updated for D11 (#1125)

--- tests/src/FunctionalJavascript/Form/AuthenticationFormJsTest.php
+++ tests/src/FunctionalJavascript/Form/AuthenticationFormJsTest.php
@@ -37,7 +37,7 @@ class AuthenticationFormJsTest extends ApigeeEdgeFunctionalJavascriptTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'classy';
+  protected $defaultTheme = 'olivero';
 
   /**
    * Valid username.
