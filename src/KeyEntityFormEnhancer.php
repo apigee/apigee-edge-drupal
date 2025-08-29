61220d3717894bd4331dac860f2132894e24a5fe
Kedar Khaire <kwkhaire11@gmail.com>
2025-08-29 18:48:12 +0530

    Updated condition for hybrid instance

--- src/KeyEntityFormEnhancer.php
+++ src/KeyEntityFormEnhancer.php
@@ -349,77 +349,86 @@ final class KeyEntityFormEnhancer {
       // Test the connection.
       $this->connector->testConnection($test_key);
       $this->messenger()->addStatus($this->t('Connection successful.'));
+
+      $input_values = $form_state->getValues();
+      // Data Residency check.
       $key_value = json_decode($key_value, TRUE);
-      // Converting Json string to array.
-      $json_array = json_decode($key_value['account_json_key'], TRUE);
-      // Converting Json array to json string for file data save.
-      $json_content = json_encode($json_array, JSON_PRETTY_PRINT);
-      $fileSystem = \Drupal::service('file_system');
-      $directory = $fileSystem->realpath("private://.apigee_edge");
-      $fileSystem->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
-      $fileLocation = $directory . '/apigeegcpacckey.json';
-      $fileSystem->saveData($json_content, $fileLocation, FileExists::Replace);
+      if ($input_values['instance_type'] == EdgeKeyTypeInterface::INSTANCE_TYPE_HYBRID) {
+        // Converting Json string to array.
+        $json_array = json_decode($input_values['account_json_key'], TRUE);
+        // Converting Json array to json string for file data save.
+        $json_content = json_encode($json_array, JSON_PRETTY_PRINT);
+        $fileSystem = \Drupal::service('file_system');
+        $directory = $fileSystem->realpath("private://.apigee_edge");
+        $fileSystem->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+        $fileLocation = $directory . '/apigeegcpacckey.json';
+        $fileSystem->saveData($json_content, $fileLocation, FileExists::Replace);
 
-      $scopes = [ClientInterface::APIGEE_TOKEN_ENDPOINT];
-      // Path to your service account key JSON file.
-      // IMPORTANT: Secure this file!
-      $serviceAccountKeyFilePath = $fileLocation;
-      try {
-        // --- Initialize Google Client ---
-        $client = new GoogleClient();
-        $client->setApplicationName("GCP Project Mapping Fetcher");
-        $client->setAuthConfig($serviceAccountKeyFilePath);
-        $client->setScopes($scopes);
+        $scopes = [ClientInterface::APIGEE_TOKEN_ENDPOINT];
+        // Path to your service account key JSON file.
+        // IMPORTANT: Secure this file!
+        $serviceAccountKeyFilePath = $fileLocation;
+        try {
+          // --- Initialize Google Client ---
+          $client = new GoogleClient();
+          $client->setApplicationName("GCP Project Mapping Fetcher");
+          $client->setAuthConfig($serviceAccountKeyFilePath);
+          $client->setScopes($scopes);
 
-        // --- Fetch the Access Token ---
-        $accessToken = $client->fetchAccessTokenWithAssertion();
-        if (isset($accessToken['access_token'])) {
-          $curlUrl = ClientInterface::APIGEE_ON_GCP_ENDPOINT . "/organizations/" . $key_value['organization'] . ":getProjectMapping";
-          $ch = curl_init();
-          // Set cURL options.
-          curl_setopt($ch, CURLOPT_URL, $curlUrl);
-          // Return the transfer as a string.
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Authorization: Bearer ' . $accessToken['access_token'],
-            'Content-Type: application/json',
-          ]);
+          // --- Fetch the Access Token ---
+          $accessToken = $client->fetchAccessTokenWithAssertion();
+          if (isset($accessToken['access_token'])) {
+            $curlUrl = ClientInterface::APIGEE_ON_GCP_ENDPOINT . "/organizations/" . $input_values['organization'] . ":getProjectMapping";
+            $ch = curl_init();
+            // Set cURL options.
+            curl_setopt($ch, CURLOPT_URL, $curlUrl);
+            // Return the transfer as a string.
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+              'Authorization: Bearer ' . $accessToken['access_token'],
+              'Content-Type: application/json',
+            ]);
 
-          // Execute cURL request and get the response.
-          $response = curl_exec($ch);
+            // Execute cURL request and get the response.
+            $response = curl_exec($ch);
 
-          // Check for cURL errors.
-          if (curl_errno($ch)) {
-            $this->messenger()->addError($this->t('cURL error: @error', ['@error' => curl_error($ch)]));
-          }
-          else {
-            // Process the cURL response.
-            $decoded_response = json_decode($response, TRUE);
-            if (isset($decoded_response['location'])) {
-              $classLocation = 'APIGEE_ON_GCP_' . strtoupper($decoded_response['location']) . '_DRZ_ENDPOINT';
-              $this->messenger()->addStatus($this->t('Data residency is enabled for this organization. Service endpoint being used is @serviceEndpoint', ['@serviceEndpoint' => constant(ClientInterface::class . '::' . $classLocation)]));
-              $key_value['drzlocation'] = $decoded_response['location'];
+            // Check for cURL errors.
+            if (curl_errno($ch)) {
+              $this->messenger()->addError($this->t('cURL error: @error', ['@error' => curl_error($ch)]));
             }
             else {
-              unset($key_value['drzlocation']);
+              // Process the cURL response.
+              $decoded_response = json_decode($response, TRUE);
+              if (isset($decoded_response['location'])) {
+                $classLocation = 'APIGEE_ON_GCP_' . strtoupper($decoded_response['location']) . '_DRZ_ENDPOINT';
+                $this->messenger()->addStatus($this->t('Data residency is enabled for this organization. Service endpoint being used is @serviceEndpoint', ['@serviceEndpoint' => constant(ClientInterface::class . '::' . $classLocation)]));
+                $key_value['drzlocation'] = $decoded_response['location'];
+                $input_values['drzlocation'] = $decoded_response['location'];
+              }
+              else {
+                unset($key_value['drzlocation']);
+                unset($input_values['drzlocation']);
+              }
             }
+            // Close cURL resource.
+            curl_close($ch);
+            $form_state->setValues(['key_value' => json_encode(array_filter($key_value))]);
           }
-          // Close cURL resource.
-          curl_close($ch);
-          $form_state->setValues(['key_value' => json_encode(array_filter($key_value))]);
+          else {
+            $this->messenger()->addStatus($this->t('Failed to fetch access token.\n'));
+            // Print full response for debugging.
+            $this->messenger()->addStatus($accessToken);
+            unset($key_value['drzlocation']);
+          }
         }
-        else {
-          $this->messenger()->addStatus($this->t('Failed to fetch access token.\n'));
-          // Print full response for debugging.
-          $this->messenger()->addStatus($accessToken);
-          unset($key_value['drzlocation']);
+        catch (\Exception $e) {
+          echo "An error occurred: " . $e->getMessage() . "\n";
+          if ($e->getPrevious()) {
+            echo "Previous error: " . $e->getPrevious()->getMessage() . "\n";
+          }
         }
-      }
-      catch (\Exception $e) {
-        echo "An error occurred: " . $e->getMessage() . "\n";
-        if ($e->getPrevious()) {
-          echo "Previous error: " . $e->getPrevious()->getMessage() . "\n";
-        }
+      } else {
+        unset($input_values['drzlocation']);
       }
 
       // Based on type of organization, cache needs to clear.
