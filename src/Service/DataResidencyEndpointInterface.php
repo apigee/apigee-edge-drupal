bd3aafceb511c24a4ffa597d5234ee5f8fd7ae69
divyajose <divyajose@google.com>
2025-09-12 14:48:41 +0530

    Drz changes

---
+++ src/Service/DataResidencyEndpointInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright 2025 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+namespace Drupal\apigee_edge\Service;
+
+use Drupal\key\KeyInterface;
+
+/**
+ * Defines an interface for discovering the data residency endpoint.
+ */
+interface DataResidencyEndpointInterface {
+
+  /**
+   * The key used to store the endpoint in the state.
+   */
+  public const DRZ_ENDPOINT = 'drzendpoint';
+
+  /**
+   * Discovers and stores the data residency endpoint for a given key.
+   *
+   * @param \Drupal\key\KeyInterface $key
+   *   The key to discover the endpoint for.
+   */
+  public function getEndpoint(KeyInterface $key): void;
+
+}
