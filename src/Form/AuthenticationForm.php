b8b677831dd7f1279fac212109ac3a0de8a7b4dc
Kedar Khaire <kedark@google.com>
2025-10-01 13:46:42 +0530

    Added save() and changed  to Drupal (#1186)

--- src/Form/AuthenticationForm.php
+++ src/Form/AuthenticationForm.php
@@ -184,7 +184,7 @@ class AuthenticationForm extends KeyEditForm {
     $status = parent::save($form, $form_state);
 
     // Save the authentication key entity id to module's configuration.
-    $this->configFactory->getEditable(static::CONFIG_NAME)->set('active_key', $this->entity->id());
+    \Drupal::configFactory()->getEditable(static::CONFIG_NAME)->set('active_key', $this->entity->id())->save();
     // Override the redirect destination.
     $form_state->setRedirect('apigee_edge.settings');
 
