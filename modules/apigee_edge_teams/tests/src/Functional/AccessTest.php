d16b8c518884deb35a27e731d3ee6fe1f2c161dc
shailesh-google <shail@google.com>
2026-01-20 21:52:30 +0530

    Fixing Performance issue of team page by Adding cache in 4.x (#1211)
    * Fixing Performance issue of team page by Adding cache in 4.x
    
    * Fixed Issue: Team Listing Page Existing teams disappear after adding

--- modules/apigee_edge_teams/tests/src/Functional/AccessTest.php
+++ modules/apigee_edge_teams/tests/src/Functional/AccessTest.php
@@ -209,6 +209,12 @@ class AccessTest extends ApigeeEdgeTeamsFunctionalTestBase {
     $this->teamMembershipManager = $this->container->get('apigee_edge_teams.team_membership_manager');
     $this->teamPermissionHandler = $this->container->get('apigee_edge_teams.team_permissions');
     $this->state = $this->container->get('state');
+    // This state acts as a "kill switch." Our overridden
+    // `getFromPersistentCache()` method checks for this state.
+    // When it's TRUE, that method will *always* return [], forcing
+    // a cache miss and guaranteeing that every test gets fresh data
+    // from the source (the API) instead of the cache.
+    $this->state->set('apigee_teams_test_skip_cache', TRUE);
 
     $team_entity_type = $this->container->get('entity_type.manager')->getDefinition('team');
     $team_app_entity_type = $this->container->get('entity_type.manager')->getDefinition('team_app');
